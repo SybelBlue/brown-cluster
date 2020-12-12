e701f3164b1b67af44ed2066597eff1b30a8a0c6
jcrayton <jcrayton@macalester.edu>
2020-12-12 10:18:28 -0600

    redid edge weighting

--- multiTree.py
+++ multiTree.py
@@ -11,16 +11,16 @@ class MultiTreeBuilder:
         """ input_file_name is the name of the file (without the extension) that contains the corpus given to Brown's algorithm
             cluster_sizes is the list of -c arguments provided to each execution of the algorithm"""
         self.cluster_sizes = cluster_sizes
-        
+
         # file locations of the paths files
-        self.file_names = [f"{input_file_name}-c{size}-p1.out/paths" for size in cluster_sizes] 
+        self.file_names = [f"{input_file_name}-c{size}-p1.out/paths" for size in cluster_sizes]
         for f_name in self.file_names:
             if not exists(f_name):
                 raise ValueError(f'File at {f_name} could not be found')
-        
+
         # TreeBuilder's buildTree method takes in a file location and creates a tree.
         self.tree_builders = { path: self.make_new_tree(path) for path in self.file_names }
-        
+
         self.word_paths = defaultdict(list) # stores the bitstring paths for each word
         self.trees = list() # list of trees
 
@@ -56,7 +56,7 @@ class MultiTreeBuilder:
         #         maxPath = longest distance between 2 nodes, longest walk in the tree
         #         abPath = length of path between a,b
         #         edgeWeight += ((maxPath + 1)/(abPath + 1))*C
-        
+
         def make_bitstring_key(tree_num, str0, str1):
             """makes it so that each pair of nodes in a tree has a unique label regardless of ordering"""
             major = max(str0, str1)
@@ -89,15 +89,20 @@ class MultiTreeBuilder:
 
     def pairwise_score_iterable(self):
         max_tree_depths = [builder.max_depth() for builder in self.tree_builders.values()]
-        
+
         for (a, a_bitstrs), (b, b_bitstrs) in combinations(self.word_paths.items(), 2):
             edge_weight = 0
             for i, depth in enumerate(max_tree_depths):
                 ab_path = TreeBuilder.distance(a_bitstrs[i], b_bitstrs[i])
                 max_path = 2 * depth
                 edge_weight += ((max_path + 1) / (ab_path + 1)) * self.cluster_sizes[i]
+
+                # So here's the thing
+                # they're really closer together when they have smaller edge weights
+                # duh, im just an idiot
+                edge_weight = ab_path * self.cluster_sizes[i]
             yield (a, b), edge_weight
-                
+
 
 if __name__ == "__main__":
     cluster_flag = LiteralFlag('c', 'clusters', 'List of cluster sizes to compare')
@@ -115,9 +120,9 @@ if __name__ == "__main__":
         raise ValueError('Unkown args: ', *args)
 
     # now the args are parsed
-    # cluster_flag should have a list of cluster sizes as cluster_flag.value 
+    # cluster_flag should have a list of cluster sizes as cluster_flag.value
     # input_name should be a string with the name of the input file for the original brown's algorithm
-    
+
     multi_builder = MultiTreeBuilder(input_name, cluster_flag.value)
     multi_builder.build_all()
 
@@ -130,5 +135,5 @@ if __name__ == "__main__":
             f.write(',')
             f.write(str(weight))
             f.write('\n')
-    
+
     print('done')
